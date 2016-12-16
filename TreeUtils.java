2575f4494443f2b39914372cd8b6cef9248e83a3
TheSeventhBullet <antoniolpa.amorim@gmail.com>
2016-12-16 15:28:30 +0000

    Update TreeUtils.java

--- TreeUtils.java
+++ TreeUtils.java
@@ -3,7 +3,7 @@ package series.serie3;
 import series.serie2.Node;
 
 import java.util.Comparator;
-
+import java.security.InvalidParameterException;
 /**
  * Created by Pedro on 09/12/2016.
  */
@@ -50,7 +50,18 @@ public class TreeUtils {
     }
 
     public static Node<Integer> createBSTFromRange(int start, int end){
-        return null;
+        if (end > start)
+            throw new InvalidParameterException();
+        int mid = (start + end) / 2;
+
+        Node<Integer> midNode = new Node<>(mid);
+
+        if (mid != start)
+            midNode.previous = createBSTFromRange(start, mid - 1);
+        if (mid != end)
+            midNode.next = createBSTFromRange(mid + 1, end);
+
+        return midNode;
     }
 
     public static <E> boolean isBST(Node<E> root, Comparator<E> cmp){
@@ -72,5 +83,26 @@ public class TreeUtils {
 
         return bool;
     }
+    
+   //main do 1.2 
+    public static void main(String[] args) {
+        aux = createBSTFromRange(-5,5);
+        print();
+    }
 
+    private static<E> void printNode(E e, int level) {
+        for (int i = 0; i < level; ++i)
+            System.out.print("\t");
+        System.out.println(e);
+    }
+    private static<E> void printR(Node<E> t, int level) {
+        if (t == null)
+            return;
+        printR(t.next, level+1);
+        printNode(t.value, level);
+        printR(t.previous, level+1);
+    }
+    public static void print() {
+        printR(aux, 0);
+    }
 }
